7c1f0ebd106af0924f130fa4f45dafb4e012b41b
최선화 <161446039+sunhew@users.noreply.github.com>
2024-06-10 10:14:27 +0900

    02 페이지 세팅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import './assets/scss/index.scss';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-function App() {
+import Header from './components/Header'
+import Main from './components/Main'
+import Aside from './components/Aside'
+import Search from './components/Search'
+
+import Home from './pages/Home'
+import MelonPage from './pages/MelonPage'
+import BugsPage from './pages/BugsPage'
+import ApplePage from './pages/ApplePage'
+import GeniePage from './pages/GeniePage'
+import BillPage from './pages/BillPage'
+
+import Recent from './pages/Recent'
+import Favorites from './pages/Favorites'
+import Mymusic from './pages/Mymusic'
+
+
+
+const App = () => {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <Main>
+        <Search />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/melon' element={<MelonPage />} />
+          <Route path='/bugs' element={<BugsPage />} />
+          <Route path='/apple' element={<ApplePage />} />
+          <Route path='/genie' element={<GeniePage />} />
+          <Route path='/bill' element={<BillPage />} />
+          <Route path='/recent' element={<Recent />} />
+          <Route path='/favorites' element={<Favorites />} />
+          <Route path='/mymusic' element={<Mymusic />} />
+        </Routes>
+      </Main>
+      <Aside />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/BillPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BillPage = () => {
+    return (
+        <div>BillPage</div>
+    )
+}
+
+export default BillPage
